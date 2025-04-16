e834a95d2a29e8a584e58f28c190ff6227da765e
Hisham K H <hishamkhofficial@gmail.com>
2025-04-16 20:18:12 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import Home from "./pages/home";
+import Home from "./pages/Home";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import NavBar from "./components/NavBar";
 import Billing from "./pages/Billing-page";
